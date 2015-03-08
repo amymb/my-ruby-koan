6e555b901a83db56e7b4be9f03e10829a6ba0f05
Amy Bertken <amy.bertken@gmail.com>
2015-03-08 14:42:11 -0600

    finished illegal triangles throw exceptions

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,23 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a <= 0 || b <=0 || c <= 0
+    raise TriangleError, "Side can't have length 0 or less"
+  end
+  if a + b <=c || a + c <= b || b+c <=a
+    raise TriangleError, "Side can't have length 0 or less"
+  end
+  if a == b && b == c
+    return :equilateral
+  elsif a == b || b == c || a == c
+      return :isosceles
+    else
+      return :scalene
+    end
 end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+
 end
